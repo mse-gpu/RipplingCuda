179b4ebb557bd2aa90c0dbedd61ad22d6204dcc7
Baptiste Wicht <baptiste.wicht@gmail.com>
2012-05-19 14:05:09 +0200

    Finalize benchmark

--- src/cpp/main.cpp
+++ src/cpp/main.cpp
@@ -45,7 +45,8 @@ int launchApplication(int argc, char** argv){
         }
 }
 
-#define DIM_BENCH 10000
+#define DIM_BENCH 25000
+#define TIMES 25
 
 extern void useKernelAnimationHSB(uchar4* ptrDevPixels, int w, int h, float t);
 
@@ -59,9 +60,6 @@ int bench(int argc, char** argv){
     	HANDLE_ERROR(cudaSetDeviceFlags(cudaDeviceMapHost)); // Not all gpu allow the use of mapMemory (avant prremier appel au kernel)
 
     	//Force the driver to run
-    	int* fake;
-    	HANDLE_ERROR(cudaMalloc((void**) &fake, sizeof(int)));
-
     	uchar4* image;
     	HANDLE_ERROR(cudaMalloc(&image, DIM_BENCH * DIM_BENCH * sizeof(uchar4)));
 
@@ -70,26 +68,26 @@ int bench(int argc, char** argv){
 
     	CUevent start;
     	CUevent stop;
-    	HANDLE_ERROR(cudaEventCreate(&start, CU_EVENT_DEFAULT));
-    	HANDLE_ERROR(cudaEventCreate(&stop, CU_EVENT_DEFAULT));
+    	HANDLE_ERROR(cudaEventCreate(&start));
     	HANDLE_ERROR(cudaEventRecord(start, 0));
 
-    	for(int i = 0; i < 10; ++i){
+    	for(int i = 0; i < TIMES; ++i){
             useKernelAnimationHSB(image, DIM_BENCH, DIM_BENCH, 1);
     	}
 
         float elapsed = 0;
+    	HANDLE_ERROR(cudaEventCreate(&stop));
     	HANDLE_ERROR(cudaEventRecord(stop, 0));
         HANDLE_ERROR(cudaEventSynchronize(stop));
     	HANDLE_ERROR(cudaEventElapsedTime(&elapsed, start, stop));
 
-        std::cout << "CUDA Version took " << elapsed << "ms" << std::endl;
+	std::cout << "Total (" << TIMES << " times) = " << elapsed << "ms" << std::endl;
+	std::cout << "Mean  (" << TIMES << " times) = " << (elapsed / TIMES) << "ms" << std::endl;
 
     	HANDLE_ERROR(cudaEventDestroy(start));
     	HANDLE_ERROR(cudaEventDestroy(stop));
 
     	HANDLE_ERROR(cudaFree(image));
-    	HANDLE_ERROR(cudaFree(fake));
 
     	return EXIT_SUCCESS;
     } else {
